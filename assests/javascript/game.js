c8ef548af5d33221ce51352a204d4ea673d78e97
Cayde6 <stephaniementzer@gmail.com>
2018-12-13 19:20:57 -0500

    pushed for help

---
+++ assests/javascript/game.js
@@ -0,0 +1,13 @@
+
+// computer genterates a letter
+// user gets (x) guesses
+//chances
+//guesses so far...
+// did the user guess the computers letter
+
+
+    var wins = 0;
+    var losses = 0;
+    var guessesLeft = 9;
+    var guessesSoFar = []; // push users choices
+    var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; //list of letters to choose from
