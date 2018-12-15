4e3a2e63e0971d6b81be53dee2796c58919fa3ec
Cayde6 <stephaniementzer@gmail.com>
2018-12-15 12:48:31 -0500

    i dont think i pushed everything

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -6,30 +6,63 @@
 // did the user guess the computers letter
 
 //--------- variables
-    var wins = 0;
-    var losses = 0;
-    var guessesLeft = 9;
-    var guessesSoFar = []; // push users choices
-    var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; //list of letters to choose from
+var wins = 0;
+var losses = 0;
+var startingGuesses = 10;
+var guessesLeft = startingGuesses;
+var guessesSoFar = []; // push users choices
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; //list of letters to choose from
 
 
 //---------functions
- // computer makes choice    
- var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+// computer makes choice    
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log("computer guess: " + computerGuess);
 
 // user makes choice 
-document.onkeydown = function(keyboardEntry) {
-    var userGuess = keyboardEntry.key;
-    console.log(userGuess);
+document.onkeyup = function(event) {
+    var userGuess = event.key.toLowerCase();
+    console.log("user guess: " + userGuess);
+    if (!computerChoices.includes(userGuess)) {
+        alert("Press a valid key")
+       return;    
+    } 
+
+// determines the outcome
+if (userGuess === computerGuess) {
+    wins++;
+     computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log("computer guess: " + computerGuess);
+    guessesSoFar = [];
+    alert("you did it, play again?")
+}
+else if (userGuess !== computerGuess) {
+    // alert("Try Again");
+    guessesLeft--;
+    console.log("Guess left: " + guessesLeft);
+    guessesSoFar.push(" " + userGuess);
+    
+}
+if (guessesLeft <= 0) {
+    losses++;
+    guessesLeft = startingGuesses;
+    guessesSoFar = [];
+    alert("wrong, try again.");
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log("computer guess: " + computerGuess);
+    
 }
 
 
- // determines the outcome
- if ((userGuess === computerGuess)) {
-     win++;
- }
-else {
-    losses++
+
+
+document.getElementById("wins-text").innerHTML = "Wins: " + wins;
+document.getElementById("losses-text").innerHTML = "Losses: " + losses;
+document.getElementById("guessesLeft-text").innerHTML = "Guesses Left: " + guessesLeft;
+document.getElementById("lettersGuessed-text").innerHTML = "Guesses so Far: " + guessesSoFar;
+
 }
 
 //functions do something. please. 
+
+
